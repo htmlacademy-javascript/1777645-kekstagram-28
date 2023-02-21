77183345dd5511c83f650652d83fb8a6c568aacf
Dzmitry <gera.1212@yandex.ru>
2023-02-21 15:13:06 +0300

    Добавляет функцию isPalindrome

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
-const isСheckLength = (string, length) => string.length <= length;
+const isCheckLength = (string, length) => string.length <= length;
 
-isСheckLength('проверяемая строка', 18);
+const isPalindrome = (string) => {
+  const oneWord = string.toLowerCase().replaceAll(' ', '');
+  let reverseString = '';
+  for (let i = oneWord.length - 1; i >= 0; i--) {
+    reverseString += oneWord.at(i);
+  }
+  return oneWord === reverseString;
+};
+
+isCheckLength('проверяемая строка', 18);
+isPalindrome('Лёша на полке клопа нашёл ');
