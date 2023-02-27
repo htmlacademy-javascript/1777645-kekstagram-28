edef21d7701bc15412eea8c78351d72eea9dd4e7
Dzmitry <gera.1212@yandex.ru>
2023-02-27 09:52:32 +0300

    Добавляет функцию getRandomInteger

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower) + lower;
+  return Math.floor(result);
+};
+
+getRandomInteger(0, 6);
