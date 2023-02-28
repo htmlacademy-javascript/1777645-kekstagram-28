d17ad1bfda8a9db7de853b801c3f27a06c116e1d
Dzmitry <gera.1212@yandex.ru>
2023-02-28 10:33:37 +0300

    Добавляет функцию createRandomIdFromRangeGenerator

--- js/main.js
+++ js/main.js
@@ -18,3 +18,19 @@ const getRandomInteger = (min, max) => {
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
