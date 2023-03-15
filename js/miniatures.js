9bac3421f98c2ded5d13f66cefcf7f52224fec6c
Dzmitry <gera.1212@yandex.ru>
2023-03-15 12:00:40 +0300

    Исправляет замечания

--- js/miniatures.js
+++ js/miniatures.js
@@ -4,9 +4,10 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const pictureFragment = document.createDocumentFragment();
 const miniatures = createArrayPictures();
 
-miniatures.forEach(({ url, likes, comments }) => {
+miniatures.forEach(({ url, description, likes, comments }) => {
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureFragment.appendChild(pictureElement);
