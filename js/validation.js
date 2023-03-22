5039c589d052ccd95156138a37db5dbf483e2e26
Dzmitry <gera.1212@yandex.ru>
2023-03-22 12:14:01 +0300

    Изменяет проверку длины сообщения через data

--- js/validation.js
+++ js/validation.js
@@ -3,15 +3,12 @@ import { imgUploadForm } from './upload-file.js';
 const HASHTAG = /^#[a-zа-яё0-9]{1,}$/i;
 const HASHTAG_MAX_COUNT = 5;
 const HASHTAG_MAX_LENGTH = 20;
-const COMMENT_MAX_LENGTH = 140;
 const textHashtags = imgUploadForm.querySelector('.text__hashtags');
-const textDescription = imgUploadForm.querySelector('.text__description');
 const ErrorMessage = {
   NAME: 'Хэш-тег начинается с символа #, состоит из букв и чисел, и разделяется пробелом.',
   LENGTH: 'Максимальная длина одного хэш-тега 20 символов, включая решётку.',
   COUNT: 'Нельзя указать больше 5 хэш-тегов.',
   DUPLICATE: 'Один и тот же хэш-тег не может быть использован дважды.',
-  MESSAGE: 'Длина комментария не может составлять больше 140 символов.'
 };
 
 const pristine = new Pristine(imgUploadForm, {
@@ -43,7 +40,6 @@ const checkHashtagDublicates = (value) => {
   return new Set(hashtagArray).size === hashtagArray.length;
 };
 
-const checkComment = (value) => value.length <= COMMENT_MAX_LENGTH;
 
 pristine.addValidator(textHashtags, checkHashtagName, ErrorMessage.NAME);
 
@@ -52,8 +48,6 @@ pristine.addValidator(textHashtags, checkHashtagLength, ErrorMessage.LENGTH);
 pristine.addValidator(textHashtags, checkHashtagCount, ErrorMessage.COUNT);
 
 pristine.addValidator(textHashtags, checkHashtagDublicates, ErrorMessage.DUPLICATE);
-
-pristine.addValidator(textDescription, checkComment, ErrorMessage.MESSAGE);
 
 imgUploadForm.addEventListener('submit', (evt) => {
   if (!pristine.validate()) {
