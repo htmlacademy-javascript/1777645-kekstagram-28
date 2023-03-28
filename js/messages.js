5998f3a89f7a952d890aaeac138ae77cd8eded84
Dzmitry <gera.1212@yandex.ru>
2023-03-28 17:33:10 +0300

    Добавляет сообщение при загрузке данных

---
+++ js/messages.js
@@ -0,0 +1,8 @@
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.classList.add('error-message');
+  alertContainer.textContent = message;
+  document.body.append(alertContainer);
+};
+
+export { showAlert };
