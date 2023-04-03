ca10a2840315f57540c6eafe9928ddf0cd6c602b
Dzmitry <gera.1212@yandex.ru>
2023-04-03 13:37:05 +0300

    Подключает фильтр

--- js/api/get-data.js
+++ js/api/get-data.js
@@ -4,11 +4,16 @@ import { setUserFormSubmit } from './set-form.js';
 import { closeUsersModal } from '../upload-file.js';
 import { getData } from './api.js';
 import { showAlert } from '../messages.js';
+import { onFilterClick } from '../filter.js';
+import { debounce } from '../util.js';
+
+const RERENDER_DELAY = 500;
 
 getData()
   .then((miniatures) => {
     createMiniatures(miniatures);
     checkImageData(miniatures);
+    onFilterClick(miniatures, debounce(createMiniatures, RERENDER_DELAY));
   })
   .catch((err) => {
     showAlert(err.message);
